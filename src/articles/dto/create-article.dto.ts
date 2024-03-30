4f777b3d5a8de092146210cfc50732f2d6f72d87
IdirX69 <adil.idirr@gmail.com>
2024-03-30 17:18:11 +0100

    dto articles

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,22 @@
-export class CreateArticleDto {}
+// product.dto.ts
+
+import { IsString, IsNumber, IsOptional, IsArray } from 'class-validator';
+
+export class CreateArticleDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  @IsOptional()
+  readonly description?: string;
+
+  @IsNumber()
+  readonly price: number;
+
+  @IsString()
+  readonly image: string;
+
+  @IsArray()
+  @IsOptional()
+  readonly categoryIds?: number[];
+}
